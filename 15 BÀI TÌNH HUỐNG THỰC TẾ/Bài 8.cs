14766fc34d1fe60732a8e675a6d99409d709c2c1
synhann <synhanngo1706@gmail.com>
2026-08-31 02:53:16 +0700

    da hoan thanh

---
+++ 15 BÀI TÌNH HUỐNG THỰC TẾ/Bài 8.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Mã OTP nhận được (839201):");
+            DateTime CreationTime = DateTime.Now;
+            int dubai = Convert.ToInt32(Console.ReadLine());
+            if (dubai == 839201)
+            {
+                DateTime alibaba = DateTime.Now;
+                TimeSpan alamove = CreationTime - alibaba;
+                if (alamove.TotalSeconds <= 10)
+                {
+                    Console.WriteLine("THÀNH CÔNG -Giao dịch đã được phê duyệt.");
+                }
+                else
+                {
+                    Console.WriteLine("Mã OTP của bạn đã hết hạn.");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Định dạng không hợp lệ hoặc mã bị sai.");
+            }    
+
+            Console.ReadKey();
+        }
+
+    }
+}
