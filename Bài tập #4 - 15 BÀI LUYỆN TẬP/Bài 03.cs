6a00be59d4d4ba6cae69f0464e891a64cb5b9e66
synhann <synhanngo1706@gmail.com>
2026-09-10 15:58:33 +0700

    hi

---
+++ Bài tập #4 - 15 BÀI LUYỆN TẬP/Bài 03.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Text;
+namespace Bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+           
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
