29ed1c55fbed969280a4f577a5f64716a47b7d0a
synhann <synhanngo1706@gmail.com>
2026-09-10 14:27:52 +0700

    dahoanthanh

---
+++ Bài tập #4 - 15 BÀI LUYỆN TẬP/Bài 02.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Text;
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Nhập role của bạn: ");
+            string role = Console.ReadLine();
+
+            switch (role)
+            {
+                case "ADMIN":
+                    Console.WriteLine("[THÔNG BÁO: Toàn quyền quản trị hệ thống.");
+                    break;
+                case "MANAGER":
+                    Console.WriteLine("[THÔNG BÁO: Quyền quản lý nhân sự và xem báo cáo.");
+                    break;
+                case "EMPLOYEE":
+                    Console.WriteLine("[THÔNG BÁO: Quyền tạo và chỉnh sửa hồ sơ cá nhân.");
+                    break;
+                case "GUEST":
+                    Console.WriteLine("[THÔNG BÁO: Chỉ có quyền xem thông tin công khai.");
+                    break;
+            }    
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
